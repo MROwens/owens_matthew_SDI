cd0f0e81f29ac1a3d283073c13de6b6bee79ae10
Matthew Owens <Wulfy65@gmail.com>
2015-03-05 14:51:52 -0500

    Day 2 Intro
    Learned multi lined comments, single lined comment, and alert.

---
+++ Day_2/js/script.js
@@ -0,0 +1,26 @@
+/*
+Matt Owens
+3/5/15
+Section 00
+Day 2 Intro
+ */
+
+//alert("Java");
+
+/*
+Example of a multi-lined comment
+Anything inside of here will not be seen by the computer
+ */
+
+//This is an example of a single lined comment. Only this line will be ignored
+
+//alert boxes- pop up box that will alert the user with information
+
+//alert("Text that the user will see.")
+
+
+
+
+
+
+
