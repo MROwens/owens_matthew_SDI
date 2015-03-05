1e85e8e58e275fb2ecf3cfbe1da110ca3f7e2f26
Matthew Owens <Wulfy65@gmail.com>
2015-03-05 15:42:27 -0500

    Defining Variables
    Learning about defining variables.

---
+++ Day3LabExpressions/js/Day3LabExpressions.js
@@ -0,0 +1,21 @@
+/*
+Matt Owens
+3/5/15
+Section 00
+Day 3 Lab Expressions
+ */
+
+//alert("Java.");
+
+//Slice of pie part one.
+var slicesPerBox=8;
+var people=4;
+var pizzaOrdered=3;
+
+
+
+console.log("Each person ate "+results+" slices of pizza at the party.");
+
+//Slice of pie part two.
+
+
--- Day_2/js/script.js
+++ Day_2/js/script.js
@@ -18,7 +18,35 @@ Anything inside of here will not be seen by the computer
 
 //alert("Text that the user will see.")
 
+//Console.log - Shows info to the programmer
+//Great for checking values and debugging
+//Also shows all errors
 
+console.log("This is the console.");
+
+//Declare a variable
+//Use the keyword var
+
+var whatever1;
+
+//Define the variable
+//Sets the value of the existing variable
+
+whatever1=42;
+
+//Console.log the variable
+
+console.log("The variable whatever1 is");
+console.log(whatever1);
+
+//Declare and define a variable at the same time.
+
+var a=2;
+
+console.log(a);
+
+a=a+3;
+console.log(a);
 
 
 
