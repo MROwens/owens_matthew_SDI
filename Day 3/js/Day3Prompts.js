64b19255d0f2c9a9fd13db639f56510067c7a6e9
Matthew Owens <Wulfy65@gmail.com>
2015-03-07 15:27:43 -0500

    Prompts
    Learned to prompt. Be specific and make results matter.

---
+++ Day 3/js/Day3Prompts.js
@@ -0,0 +1,32 @@
+/*
+Matt Owens
+3/7/15
+Section 00
+Day 3 Prompts
+ */
+//alert("Java Works");
+
+//Ask the user for input
+//prompt("Text that the user will see.")
+
+var userInput=prompt("Enter year of birth.");
+console.log(userInput);
+
+//Calculate the area of a rectangle
+//Length * Width
+
+//Prompt the user for the width and height
+//Explain what is going on
+var height=prompt("Lets calculate the area of a rectangle. \nPlease input the height of the rectangle.");
+var width=prompt("Please input the width of the rectangle.");
+
+//Calulate the area of a rectangle
+var area=height*width;
+console.log("The area of the rectangle with a height of "+height+" and width of "+width+" is "+area+".");
+
+var results="The results of the area are "+area;
+console.log(results);
+
+
+
+
