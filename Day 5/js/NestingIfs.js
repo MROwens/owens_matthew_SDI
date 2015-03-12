bcb2d3a9f50aa00639ab5291456d63ad42df8c92
Matthew Owens <Wulfy65@gmail.com>
2015-03-12 16:24:40 -0400

    Prompt validation
    Prompt validation

--- Day 5/js/NestingIfs.js
+++ Day 5/js/NestingIfs.js
@@ -12,17 +12,51 @@ Nesting If Statements
 //If it is warm lets go to the beach, if it is not lets go to the movies
 
  //Get temp
- var temp=75;
+ var temp=prompt("We are going to figure out what you should do today. \n What is the current temperature?");
 
- //Lets set the water temp
- var waterTemp=78;
+ //Validate that the user typed in a number.
+ //isNaN() - is () not a number
+
+
+
+ if(isNaN(temp)){
+     //Validating prompts
+     temp=prompt("Please to be only entering numbers. \n What is the temperature outside?")
+ }
+
+ //Validate that the user typed in something
+ if(temp===""){
+     temp=prompt("Please to not be leaving blank. \n What is the current temperature outside.")
+ }
+
+
+
+
+
+
+
+
 
 
 
  if(temp>=80){
      console.log("Lets go to the beach.");
+     //Lets set the water temp
+     var waterTemp=prompt("What is the temperature of the water?");
+     //validate water temp
+     if(waterTemp===""||isNaN(waterTemp)){
+         //This code will run if the variable is blank or NaN
+         //Reprompt the user
+         waterTemp=prompt("Please enter and number and do not leave blank. \n What is the water temp.")
+     }
+
      if(waterTemp>75){
          console.log("Lets get in the water.")
+         var knowSwim=propmt("Do you know how to swim?")
+
+         if(knowSwim !="yes" && knowSwim !="Yes" && knowSwim!="no")
+         knowSwim=prompt("Only type in yes or no. \n Can you swim?")
+
      }else{
          console.log("Lets get a tan.")
      }
@@ -31,7 +65,15 @@ Nesting If Statements
  }
 
  //Do we have any kids in the group
- var kids="no";
+ var kids=prompt("Are you bringing kids?");
+ //Validate
+ //Convert to lower case
+ kids=kids.toLowerCase();
+ if(kids!="yes" && kids!="no"){
+     //Re-prompt
+     kids=prompt("Please enter only yes or no. \n Are you bringing kids?")
+     kids=kids.toLowerCase();
+ }
 
  //Test if we have kids
  if(kids ==="no"){
