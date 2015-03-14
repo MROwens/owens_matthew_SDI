094d3d447a8b8c453a7f9270e4e1281ccd873ca1
Matthew Owens <Wulfy65@gmail.com>
2015-03-14 16:34:22 -0400

    While and Do While Loops
    While and Do While Loops

--- Day 6 While Loops/js/Day6WhileLoops.js
+++ Day 6 While Loops/js/Day6WhileLoops.js
@@ -1,8 +1,43 @@
 
  /*
 Matt Owens
-3/7/15
+3/14/15
 Section 00
-Day 3 Prompts
+Day 6 While Loops
  */
-alert("Java Works");
+//alert("Java Works");
+
+
+ //Basic while loops
+ //Start a counter var
+ var counter=0;
+
+ //Start the while loop
+ //Condition to test goes inside ()
+ while(counter<=200){
+     console.log("Somethings never end!");
+     console.log(counter);
+
+     //Make a change to the counting variable
+     counter+=50;
+ }
+
+
+ //Do while Loop
+ //This loop will run the code first, then check a condition
+
+ /*
+ do{
+ Code to run
+ } while(condition to test) ;
+
+  */
+
+ //Declare a counting var
+ var i=0;
+
+ do{
+     console.log("The number is "+i+".");
+     //Change the counting var
+     i++;
+ } while(i<=10);
