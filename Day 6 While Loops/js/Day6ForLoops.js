919edc6748b71d38a2b0c1fbf452380827c797f3
Matthew Owens <Wulfy65@gmail.com>
2015-03-14 16:58:56 -0400

    For loops
    For loops

--- Day 6 While Loops/js/Day6ForLoops.js
+++ Day 6 While Loops/js/Day6ForLoops.js
@@ -1,8 +1,41 @@
 
  /*
 Matt Owens
-3/7/15
+3/14/15
 Section 00
-Day 3 Prompts
+Day 6 For Loops
  */
-alert("Java Works");
+//alert("Java Works");
+
+
+ // for(initialization; condition; increment of change){}
+ // we use i a lot in loops - stands for iteration - time around
+
+ for(var i=0; i<=20; i++){
+     console.log("The value of i is "+i+".");
+ }
+
+ //Break in a loop
+
+ for(var j=0; j<5; j++){
+     //Condidtional to see if you can break the loop
+     //If the value j is 3, then console.log something differnet
+     console.log("The value of j is "+j+".");
+
+     if(j===3){
+         console.log("j is 3");
+         break;
+     }
+ }
+
+ //Specific cases for each value
+
+ for(var k=0; k<3; k++){
+     if(k===0){
+         console.log("The value is zero.");
+     }else if(k===1){
+         console.log("The value is one.");
+     }else if(k===2){
+         console.log("The value is two.");
+     }
+ }
