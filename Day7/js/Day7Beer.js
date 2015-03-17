4d8ba1ac0d873507643bc66d9c9b6f7130c7794b
Matthew Owens <Wulfy65@gmail.com>
2015-03-17 13:52:46 -0400

    Bottles of Beer on the wall
    Bottles of Beer on the wall

--- Day7/js/Day7Beer.js
+++ Day7/js/Day7Beer.js
@@ -8,4 +8,19 @@ Day 7 Beer
 //alert("Java Works");
 
 
- //
+ //Code the song 99 bottles of beer on the wall
+
+ //Basic structure of for loop
+ //for(var i, ; condition to test; increment of change ){}
+
+ for(var i=99; i>0; i--) {
+
+     if(i===1){
+         console.log("1 bottle of beer on the wall. 1 bottle of beer. You take one down, pass it around, no more bottles of beer on the wall.\nThe end.")
+
+     }else{
+         console.log(i+" bottles of beer on the wall. "+i+" bottles of beer. You take one down, pass it around, "+i-1+" bottles of beer on the wall.")
+
+     }
+
+ }
