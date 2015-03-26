33b27f9dc581f7536839a3281f6de0fee3b64431
Matthew Owens <Wulfy65@gmail.com>
2015-03-26 18:29:52 -0400

    Final Submission Functions Assignment 2
    Added a second test value

--- Functions Assignment/js/FunctionsAssignment.js
+++ Functions Assignment/js/FunctionsAssignment.js
@@ -76,4 +76,7 @@ console.log("You are spending $"+spending+" monthly on Starbucks. That translate
  /*
  I entered an initial value of 4.99 then 5 for the days of the week and got an output of $19.98 a month. I input the 19.98 to calculate the yearly cost and got $239.76.
  For a final output of "You are spending $19.98 a month on Starbucks. This translates to $239.76 a year.
+
+  I entered an initial value of 7.99 then 3 for the days of the week and got an output of $19.98 a month. I input the $31.97 to calculate the yearly cost and got $383.64.
+  For a final output of "You are spending $19.98 a month on Starbucks. This translates to $382.64 a year."
   */
